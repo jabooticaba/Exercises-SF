61d927925d1995f42fe2806cc8c0cc8deef2cb26
Vladimir Gorbovskiy <80061175+jabooticaba@users.noreply.github.com>
2021-04-12 21:10:49 +0700

    Update 13.8.9.py

--- 13.8.9.py
+++ 13.8.9.py
@@ -1,11 +1,11 @@
 count = int(input('Участников конференции: '))
 price = 0
 for i in range(count):
-    age = int(input(f'Введите возраст участника {i}: ')) # TODO Вставить счётчик
+    age = int(input(f'Введите возраст участника {i}: ')) # TODO Вставить счётчик, i не работает?
     if 18 <= age <= 24:
         price += 990
     elif age > 24:
         price += 1390
-if count >= 3:
+if 3 <= count:
     price = price * 0.9
 print (round(price))
