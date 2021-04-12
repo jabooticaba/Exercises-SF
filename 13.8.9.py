ba53a2a65a065294c3cc56360deb8188b3908a7d
Vladimir Gorbovskiy <80061175+jabooticaba@users.noreply.github.com>
2021-04-12 20:53:37 +0700

    Update 13.8.9.py

--- 13.8.9.py
+++ 13.8.9.py
@@ -1,7 +1,7 @@
 count = int(input('Участников конференции: '))
 price = 0
 for i in range(count):
-    age = int(input('Введите возраст участника: ')) # TODO Вставить счётчик
+    age = int(input(f'Введите возраст участника {i}: ')) # TODO Вставить счётчик
     if 18 <= age <= 24:
         price += 990
     elif age > 24:
