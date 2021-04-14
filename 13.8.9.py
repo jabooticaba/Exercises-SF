bdd8f9aa3dc7ce710b20fc488f118fc57920a1e4
Vladimir Gorbovskiy <80061175+jabooticaba@users.noreply.github.com>
2021-04-14 18:41:22 +0700

    Update 13.8.9.py

--- 13.8.9.py
+++ 13.8.9.py
@@ -1,12 +1,12 @@
-count = int(input('Участников конференции: '))
+count = int(input('Участников конференции:'))
 price = 0
 for i in range(count):
-    print('Введите возраст участника #', i + 1, ': ', sep ='', end='') # формат строки ввода возраста участников
+    print(f'Введите возраст участника #{i + 1}: ', end='')
     age = int(input())
     if 18 <= age <= 24:
         price += 990
     elif age > 24:
         price += 1390
-if 3 <= count:
+if count >= 3:
     price = price * 0.9
-print (round(price))
+print(f'Сумма к оплате: {round(price)}')
