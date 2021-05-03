2f98b775fbe578f2a234b869a63fac976ca6d393
Vladimir Gorbovskiy <80061175+jabooticaba@users.noreply.github.com>
2021-05-03 21:32:44 +0700

    Update pet_constructor.py

--- pet_constructor.py
+++ pet_constructor.py
@@ -4,7 +4,7 @@ class Cat:
         self.gender = gender
         self.name = name
         
-    def get_a(self):
+    def __str__(self):
         return f"Name: {self.name} \nSex: {self.gender} \nAge: {self.age}"
 #        return "Имя: {0} \nпол: {1} \nвозраст: {2}".format(self.name, self.gender, self.age)
-        
+#        return self.name, self.gender, self.age        
