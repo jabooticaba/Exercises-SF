02bee22cbbafc3b5a37cb1fe5b5ecb512d245eb1
Vladimir Gorbovskiy <80061175+jabooticaba@users.noreply.github.com>
2021-05-03 21:35:28 +0700

    Update cat.py

--- cat.py
+++ cat.py
@@ -3,7 +3,7 @@ from pet_constructor import Cat
 cat1 = Cat(2, 'мальчик', 'Барон')
 cat2 = Cat(2, 'мальчик', 'Сэм')
 
-print(cat1.get_a())
+print(cat1.get_a())  # Вызов метода get_a класса Cat
 print(cat2.get_a())
 
-# print(vars(cat1))
+print(vars(cat1))  # возвращает все атрибуты объекта в виде словаря
