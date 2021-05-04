ce4d6f24ba7133a7880860ed8d1065c5dd9b4106
Vladimir Gorbovskiy <80061175+jabooticaba@users.noreply.github.com>
2021-05-05 01:11:30 +0700

    Update 16.9.py

--- 16/16.9.py
+++ 16/16.9.py
@@ -21,3 +21,61 @@ print(tri)
 
 
 # 16.9.2
+
+class Rectangle:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def get_area(self):
+        return self.a * self.b
+
+rect = Rectangle(5,10)
+
+print(rect.get_area())
+
+# 16.9.3
+
+class Client:
+    className = "Клиент"
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance = balance
+
+    def display(self):
+        return f'{self.className} "{self.name}". Баланс: {self.balance} руб'
+
+client1 = Client("Иван Петров", 50)
+
+print(client1.display())
+
+
+# 16.9.4
+
+def id_generator():
+    for i in range(1, 10001):
+        yield i
+
+
+gen = id_generator()
+
+
+class Employee:
+    def __init__(self, name, id =''):
+        self.name = name
+        self.id = id
+
+
+class Volunteer(Employee):
+    def __init__(self, name, city, status):
+        self.city = city
+        self.status = status
+        Employee.__init__(self, name, "E" + str(next(gen)))
+
+    def display(self):
+        return f'{self.name}, г.{self.city}, статус "{self.status}"'
+
+
+vol = Volunteer("Иван Петров", "Москва", "Наставник")
+
+print(vol.display())
