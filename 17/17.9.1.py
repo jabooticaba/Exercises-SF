701eff68d73acbaeed35f48f2d75022748432717
Vladimir Gorbovskiy <80061175+jabooticaba@users.noreply.github.com>
2021-05-13 01:37:48 +0700

    Update 17.9.1.py

--- 17/17.9.1.py
+++ 17/17.9.1.py
@@ -8,31 +8,32 @@ def ranging2(x):
     return x
 
 
-def search_binary(list, target, left_half, right_half):
+def search_binary(list, targ, left_half, right_half):
     """Поиск элемента target в списке list"""
     if left_half > right_half:
         return False
 
     middle = left_half + (right_half - left_half) // 2
-    if list[middle] == target:
-        if list[0] == target:
+    if list[middle] == targ:
+        if list[0] == targ:
             return f'введённое число - наименьшее в списке, его индекс: 0'
-        if list[-1] == target:
+        if list[-1] == targ:
             return f'{middle - 1}, введённое число - наибольшее в списке'
         else:
             return middle-1, middle
 
-    elif target < list[middle]:
-        return search_binary(list, target, left_half, middle - 1)
+    elif targ < list[middle]:
+        return search_binary(list, targ, left_half, middle - 1)
     else:
-        return search_binary(list, target, middle + 1, right_half)
+        return search_binary(list, targ, middle + 1, right_half)
 
+    
 try:
-    a = '1 2 3 5 6 7'
-    t = int(input())
-    b = list(map(int, a.split())) + [t]  # Преобразование в список, добавление введённого числа
+    raw_input = '1 2 3 5 6 7'
+    target = int(input())
+    format_list = list(map(int, raw_input.split())) + [target]  # Преобразование в список, добавление введённого числа
 except Exception as e:
     print("Введённые числа не соответствуют параметрам задания")
 
 else:
-    print(search_binary(ranging2(b), t, 0, len(b)))
+    print(search_binary(ranging2(format_list), target, 0, len(format_list)))
