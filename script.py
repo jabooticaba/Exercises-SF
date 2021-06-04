11b79b098633ab4c253e1e2bb2c677abaf02468a
jabooticaba <jabuticaba600@gmail.com>
2021-06-04 12:33:53 +0700

    Check-list file modified

--- script.py
+++ script.py
@@ -1,28 +1,5 @@
-def decorator_counter(func):
-    
-    def wrapper(*args):
-      wrapper.c +=1
-      return func(*args)
-    wrapper.c = 0
-    return wrapper
 
 
-@decorator_counter
-def original_func(*arg):
-  print(*arg)
-    
-original_func()
-original_func()
+a = 1000*'a'
 
-
-# def counter(fu):
-    
-#     def inner(*a,**kw):
-#         inner.count+=1
-#         return fu(*a,**kw)
-#     inner.count = 0
-#     return inner
-
-original_func("avc", "bbb")
-
-print(original_func.c)
+print(a)
