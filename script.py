bf5ae44d47126718fed179f7454bcc7513ef30fb
jabooticaba <jabuticaba600@gmail.com>
2021-08-05 23:32:06 +0700

    list+list(reversed) added

--- script.py
+++ script.py
@@ -1,19 +1,10 @@
-import pytest
+string1 = "12345"
+string2 = "abcde"
+final_string = ''
 
+all_letters = zip(list(string1)[::-1], list(string2))
 
-def is_triangle(a, b, c):
-    if (a + b <= c) or (a + c <= b) or (b + c <= a):
-        return False
-    else:
-        return True
+for letter1, letter2 in all_letters:
+    final_string = final_string + letter1 + letter2
 
-@pytest.mark.parametrize('a', [-1, 0, 1, 4], ids = ['below zero', 'zero', 'one', 'valid'])
-@pytest.mark.parametrize('b', [-1, 0, 1, 4], ids = ['below zero', 'zero', 'one', 'valid'])
-@pytest.mark.parametrize('c', [-1, 0, 1, 4], ids = ['below zero', 'zero', 'one', 'valid'])
-def test_triangle(a, b, c):
-    if a <= 0 or b <= 0 or c <= 0:
-        assert is_triangle(a, b, c) == False
-    elif (a + b <= c) or (a + c <= b) or (b + c <= a):
-        assert is_triangle(a, b, c) == False
-    else:
-        assert is_triangle(a, b, c) == True
+print(final_string)
