9b8c424f41c70de96f66038d16feed327f4f0971
Vladimir Gorbovskiy <80061175+jabooticaba@users.noreply.github.com>
2021-09-27 22:29:30 +0700

    Update rectangle.py

--- 16/rectangle.py
+++ 16/rectangle.py
@@ -1,20 +1,28 @@
 from math import pi
 
 class Rectangle:
+    
     def __init__(self, a, b):
         self.a = a
         self.b = b
+        
     def get_area(self):
         return self.a * self.b
         
+        
 class Square:
+    
     def __init__(self, a):
         self.a = a
+        
     def get_area_square(self):
         return self.a ** 2
     
+    
 class Circle:
+    
     def __init__(self, a):
         self.a = a
+        
     def get_area(self):
         return pi * self.a ** 2
